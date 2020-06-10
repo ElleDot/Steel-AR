f818b548b9f6f766b91abbf733dcb364a9c63228
E~ <elle.dot2019@gmail.com>
2020-06-10 21:37:51 +0100

    Huge UI, Content and code update
    Added so much!
    
    Lots of artwork has been added, but sadly with lots of placeholders. Oh well, they'll be replaced when the design team do some stuff.
    
    ---UI---
    -Reworked the Main Menu layout
    -Designed some buttons
    -Main Menu has nice animations now
    
    ---FEATURES---
    -Added some models
    -Streamlined accessing databases by limiting how many are used
    -Fixed scene transitions
    -Set up lots of objects for playing sounds, but not fully functioning yet
    
    ---GALLERY---
    -Gallery page now exists
    -Added some cool new scrolly business (vertical scrolling content)
    -Added Scrollbar, but will disable function, keeping looks only
    -Added buffers at the top and bottom to keep things looking nice
    
    --FILES---
    Restructured the project massively with the introduction of the Resources folder. This is just to streamline the repo/folders and it seems to be nicer so it's a win.

---
+++ Assets/NewScripts/scroller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroller : MonoBehaviour {
+
+    public GameObject backQuad;
+    public GameObject sheenQuad;
+    private Renderer backQuadRenderer;
+    private Renderer sheenQuadRenderer;
+
+    float scrollSpeed = 0.03f;
+
+    void Start() {
+        backQuadRenderer = backQuad.GetComponent<Renderer>();
+        sheenQuadRenderer = sheenQuad.GetComponent<Renderer>();
+    }
+
+    void Update() {
+        Vector2 backTextureOffset = new Vector2(0, -(Time.time * scrollSpeed));
+        Vector2 sheenTextureOffset = new Vector2(0, (Time.time * scrollSpeed*10));
+        backQuadRenderer.material.mainTextureOffset = backTextureOffset;
+        sheenQuadRenderer.material.mainTextureOffset = sheenTextureOffset;
+    }
+}
---
+++ Assets/NewScripts/scrollerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollerUp : MonoBehaviour
+{
+
+    public GameObject sheenQuad;
+    private Renderer sheenQuadRenderer;
+
+    float scrollSpeed = 0.03f;
+
+    void Start()
+    {
+        sheenQuadRenderer = sheenQuad.GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        Vector2 sheenTextureOffset = new Vector2(0, (Time.time * scrollSpeed * 5));
+        sheenQuadRenderer.material.mainTextureOffset = sheenTextureOffset;
+    }
+}
