85daf2e83be6728291806f0c00f37fdb86b0c33c
E~ <elle.dot2019@gmail.com>
2020-07-27 13:53:14 +0100

    Alpaca update and delorean AR addition

--- Assets/Scripts/ModelLoader.cs
+++ Assets/Scripts/ModelLoader.cs
@@ -49,17 +49,17 @@ public class ModelLoader : MonoBehaviour {
         if (modelName == "alpaca") {
 
             modelNameLabel.text = "Alpaca";
-            modelAuthorLabel.text = " by Example Name 1";
+            modelAuthorLabel.text = " by Ioana Parau";
             infoModelName.text = "Alpaca";
-            infoAuthorName.text = "<AuthorName>";
-            infoDescription.text = "This is an Alpaca model, provided by <name> who worked on the SteelAR project.";
+            infoAuthorName.text = "Ioana Parau";
+            infoDescription.text = "This is an Alpaca model, provided by Ioana who worked on the SteelAR project.";
 
         } else if (modelName == "tonight") {
 
             modelNameLabel.text = "'Tonight'";
-            modelAuthorLabel.text = "By Example Name 2";
+            modelAuthorLabel.text = "By Aron Spall";
             infoModelName.text = "Tonight";
-            infoAuthorName.text = "<AuthorName>";
+            infoAuthorName.text = "Aron Spall";
             infoDescription.text = "This is an animated lyric from Richard Hawley's \"Tonight the Streets are Ours\".";
 
         } else if (modelName == "forklift") {
@@ -77,6 +77,22 @@ public class ModelLoader : MonoBehaviour {
             infoModelName.text = "Astro-Bot";
             infoAuthorName.text = "Unknown";
             infoDescription.text = "This is a forklift model taken from the internet. I've no idea who made it, and this paragraph is just filler text.";
+
+        } else if (modelName == "cutlery") {
+
+            modelNameLabel.text = "Cutlery";
+            modelAuthorLabel.text = "By Unknown";
+            infoModelName.text = "Cutlery";
+            infoAuthorName.text = "Unknown";
+            infoDescription.text = "This is a sculpture created by <name> of the SteelAR team. the suspended cutlery is suspended to create the shape of a large \"S\".";
+
+        } else if (modelName == "delorean") {
+
+            modelNameLabel.text = "DeLorean";
+            modelAuthorLabel.text = "By Melvyn Ternan";
+            infoModelName.text = "DeLorean";
+            infoAuthorName.text = "Melvyn Ternan";
+            infoDescription.text = "A real throwback to the future. This model was created by Melvyn from the SteelAR team, and was hand-scanned into a 3D model!";
         }
 
     }
@@ -93,7 +109,6 @@ public class ModelLoader : MonoBehaviour {
         StartCoroutine(FadeImage(false));
 
     }
-
 
     Vector3 cachedBoxOriginalPosition = new Vector3((Screen.width/2), (Screen.height/2), 0);
     IEnumerator FadeImage(bool fadeAway){
